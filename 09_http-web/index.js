7795cf6b3f0b7ddecfdde7b438a48cbe757fd063
dertaya <2654354140@qq.com>
2023-02-21 22:50:10 +0800

    使用connect写一个简单的web

--- 09_http-web/index.js
+++ 09_http-web/index.js
@@ -10,6 +10,7 @@ http
   .createServer((req, res) => {
     if (req.url.includes("/images") && req.url.substr(-4) === ".jpg") {
       const url = __dirname + "/" + req.url;
+      // 此处还应该添加判断文件是否存在以及是否是一个文件的代码 fs.stat
       serve(url, res);
     } else if (req.url === "/") {
       serve(__dirname + "/index.html", res);
